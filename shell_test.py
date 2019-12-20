09b39ba04f768e9b7c33235ccbc1b7eac11216b0
Vincent Chantreau <43688785+VincentChantreau@users.noreply.github.com>
2019-12-20 11:51:55 +0100

    Update shell_test.py

--- shell_test.py
+++ shell_test.py
@@ -1,15 +1,38 @@
 from cmd import Cmd
 # import readline
+import json
+
 from interaction import interaction
+from time import sleep
+
+try:
+    with open('openbackup_config.json') as config:
+        config_file = json.load(config)
+except FileNotFoundError or PermissionError:
+    print("Error while reading/opening the configuration file, exiting")
+    sleep(5)
+    exit()
+
+command = interaction(
+    config_file)  # Create an interaction object to interact with the DB, passing the config file in parameters
 
 change_options = ["name", "ip"]
-command = interaction()
-list_options=["switch","router","firewall","all"] # USE A DB CALL TO RETRIEVE VALUE IN DEVICE TYPE TABLE TO BE DYNAMIC
-# list_options = command.allowed_type
+list_options = ["switch", "router", "firewall",
+                "all"]
+add_options = ["csv", "device"]
+
+
+# TODO USE A DB CALL TO RETRIEVE VALUE IN DEVICE TYPE TABLE TO BE DYNAMIC
 
 
 class Prompt(Cmd):
     prompt = "OPENBACKUP>"
+
+    def do_set_backup(self,arg):
+        command.trigger_backup(arg)
+
+    def do_set_backup_interval(self,arg):
+        command.set_backup_interval(arg)
 
     def do_list(self, arg):
         command.list(arg)
@@ -24,11 +47,23 @@ class Prompt(Cmd):
                           ]
         return completion
 
-    def do_add_device(self, arg):
-        command.add_device_to_db()
+    def do_add(self, arg):
+        if arg == "csv":
+            command.add_device_csv_list()
+        elif arg == "device":
+            command.add_device_to_db()
+        else:
+            print("Unknown option")
 
-    def do_add_csv(self, arg):
-        command.add_device_csv_list()
+    def complete_add(self, text, line, begidx, endidx):
+        if not text:
+            completion = add_options
+        else:
+            completion = [f
+                          for f in add_options
+                          if f.startswith(text)
+                          ]
+        return completion
 
     def do_change(self, arg):
 
@@ -39,8 +74,11 @@ class Prompt(Cmd):
 
             if arg == "ip":
                 command.change_device_ip()
+        else:
+            print("Unknown option")
 
     def complete_change(self, text, line, begidx, endidx):
+
         if not text:
             completion = change_options
         else:
@@ -50,7 +88,7 @@ class Prompt(Cmd):
                           ]
         return completion
 
-    def do_get_config(self,arg):
+    def do_get_config(self, arg):
         command.call_back_conf()
 
     def do_exit(self, arg):
@@ -60,26 +98,31 @@ class Prompt(Cmd):
 
     def help_list(self):
         print("This command allow you to list all devices that are registered in the Database")
-        print("You can use the parameters all, switch, router,firewall to be more or less precise")
+        print("You can use the parameters all, switch, router,firewall to be more or less specific")
         # Should add a parameter to select device category like '--switch' or '--firewall'
-
-    def help_add_device(self):
-        print("This command allow you to add a new device to the Database, by setting his IP address, name, type and location (optionnal)")
 
     def help_change(self):
         print("This command allow you to change the name or the IP address of a device in the Database")
         print("Parameters are : ip / name ")
 
-    def help_add_csv(self):
-        print("This command allow you to add multiple device at the same time with a single csv file")
+    def help_add(self):
+        print("This command takes one parameter : csv or device.")
+        print(
+            "the device option allow you to add a new device to the Database, by setting his IP address, name, type and location (optionnal)")
+        print("csv add multiple device at the same time with a single csv file")
         print("The CSV syntax must be the following one : ip_address,device_name,device_type,device_location(optional)")
 
     def help_get_config(self):
-        print("This command allow you to get back a configuration file from a device that you will be prompted to specify")
-        print("You can choose between showing the configuration on screen or save it in the DOWNLOADED CONFIGURATION folder")
+        print(
+            "This command allow you to get back a configuration file from a device that you will be prompted to specify")
+        print(
+            "You can choose between showing the configuration on screen or save it in the DOWNLOADED CONFIGURATION folder")
 
+    def help_set_backup(self):
+        print("Can take value on or off to activate the backup scheduler")
 
-    do_EOF = do_exit
+    def help_set_backup_interval(self):
+        print("Set the backup interval, can't be defined under a second")
     # Create a def for basic action like add, show, del etc, and the precise completion with another def. Can be cool
 
 
